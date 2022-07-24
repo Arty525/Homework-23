185b14f9656595d3aaf9e9f4ffebc4ea5a62c09f
Andrey <89473462+Arty525@users.noreply.github.com>
2022-07-24 15:40:26 +0300

    Add files via upload

---
+++ EX3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+
+#define CYCLE for(int i = 0; i < 10; ++i)
+
+bool capacity(int p, int c = 20) {
+	return p > 20;
+}
+
+int main() {
+	std::vector<int> carriage;
+	int passengers;
+	int total = 0;
+	std::cout << "Enter number of passengers." << std::endl;
+	CYCLE{
+		std::cout << "Carriage #" << i + 1 << ": ";
+		std::cin >> passengers;
+		total += passengers;
+		carriage.push_back(passengers);
+	}
+	std::cout << "Crowded carriages: ";
+	CYCLE{
+		if (capacity(carriage[i])) std::cout << i + 1 << " ";
+	}
+	std::cout << "Fine carriages: ";
+	CYCLE{
+		if (!capacity(carriage[i])) std::cout << i + 1 << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "Total number of passengers: " << total << std::endl;
+	return 0;
+}
