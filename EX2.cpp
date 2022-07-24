f417d735c100815e178a300f58165ac97d5f2596
Andrey <89473462+Arty525@users.noreply.github.com>
2022-07-24 14:57:46 +0300

    Add files via upload

---
+++ EX2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+#define WINTER "Winter"
+#define SPRING "Spring"
+#define SUMMER "Summer"
+#define AUTUMN "Autumn"
+
+int main() {
+#if 1
+	std::cout << WINTER << std::endl;
+#endif
+#if 0
+	std::cout << SPRING << std::endl;
+#endif
+#if 0
+	std::cout << SUMMER << std::endl;
+#endif
+#if 0
+	std::cout << AUTUMN << std::endl;
+#endif
+	return 0;
+}
